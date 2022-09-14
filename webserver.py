8d6eb459643f971c02f82ae6cb2e07aa68145251
seigo2016 <2003seigo@gmail.com>
2022-09-14 13:40:55 +0900

    update webserver (v0.2)

--- webserver.py
+++ webserver.py
@@ -1,10 +1,14 @@
+import os
 import socket
 from datetime import datetime
-
+import os
 class WebServer:
     """
     A simple Web server.
     """
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    
     def __init__(self):
         pass
 
@@ -28,22 +32,33 @@ class WebServer:
             with open("data/server_recv.txt", "wb") as f:
                 f.write(request)
 
-            response_body = "<html><body><h1>It works!</h1></body></html>"
+            request_line, remain = request.split(b"\r\n", maxsplit=1)
+            request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
 
-            response_line = "HTTP/1.1 200 OK\r\n"
+            method, path, http_version = request_line.decode().split(" ")
+
+            relative_path = path[1:]
+            static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+            try:
+                with open(static_file_path, "rb") as f:
+                    response_body = f.read()
+
+                response_line = "HTTP/1.1 200 OK\r\n"
+            except OSError:
+                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                response_line = "HTTP/1.1 404 Not Found\r\n"
 
             response_header = f"Date:{datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-            response_header += "Host: Seigo2016 Web Server/0.1\r\n"
-            response_header += f"Content-Length: {len(response_body.encode())}\r\n"
+            response_header += "Host: Seigo2016 Web Server/0.2\r\n"
+            response_header += f"Content-Length: {len(response_body)}\r\n"
             response_header += "Connection: Close\r\n"
             response_header += "Content-Type: text/html; charset=utf-8\r\n"
             
-            response = (response_line + response_header + "\r\n" + response_body).encode()
+            response = (response_line + response_header + "\r\n").encode() + response_body
 
             client_socket.send(response)
 
             client_socket.close()
-
         finally:
             server_socket.close()
             print("Server is closed")
